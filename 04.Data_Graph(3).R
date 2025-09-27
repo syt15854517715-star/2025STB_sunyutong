09b8bc18eafa573c6c530f6a8a31fa1d54238b98
syt15854517715-star <syt15854517715@gmail.com>
2025-09-27 13:17:56 +0900

    update

--- 04.Data_Graph(3).R
+++ 04.Data_Graph(3).R
@@ -4,27 +4,25 @@ View(survey)
 
 table(survey$Gender)
 
-prop.table(table(survey$Gender)) 
+ECN <- table(survey$Gender)
+prop.table(ECN) 
 
 table(survey$Gender, survey$Grade)
-prop.table(table(survey$Gender, survey$Grade), margin=1)
 
 barplot(table(survey$Nationality), col="skyblue", main="Nationality", xlab="Nationality", ylab="Count")
 
 res_tab <- table(survey$ResidentialArea)
-bp <- barplot(res_tab, horiz=TRUE, col=rainbow(length(res_tab)), main="Residential Area", xlab="Count", xlim=c(0, max(res_tab)*1.1))
+bp <- barplot(res_tab, horiz=TRUE, col=rainbow(length(res_tab)), main="Residential Area", xlab="Count",xlim=c(0, max(res_tab)*1.1))
 text(res_tab, bp, labels=res_tab, pos=4)
 
 barplot(table(survey$Gender, survey$Grade), beside=TRUE, col=c("pink","lightblue"), legend=TRUE, main="Gender by Grade") 
 
-grade_tab <- table(survey$Grade)
-pct <- round(prop.table(grade_tab)*100, 1)
-labels <- paste(names(grade_tab), " (", pct, "%)", sep="")
-pie(grade_tab, labels=labels, col=rainbow(length(grade_tab)))
+colors <- c("red","green", "blue")
+pie(table(survey$Grade), col=colors, main="Grade Distribution")
 
-hist(survey$Age, col="lightgreen", main="Age Distribution", xlab="Age")
+hist(survey$Age, main="Age Distribution", xlab="Age", col=terrain.colors(12))
 
-boxplot(Age ~ Grade, data=survey, col="lightblue", main="Age by Grade", xlab="Grade", ylab="Age")
+boxplot(Age ~ Grade, data=survey, col="yellow", main="Age by Grade", xlab="Grade", ylab="Age")
 aggregate(Age ~ Grade, data=survey, summary)
 
-plot(survey$Grade, survey$Age, pch=19, main="Grade vs Age", xlab="Grade", ylab="Age", col="blue")
+plot(survey$Grade, survey$Age, pch=19, main="Grade vs Age", xlab="Grade", ylab="Age")
